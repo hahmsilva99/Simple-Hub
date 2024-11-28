77001dce25b8e1a23bb0981d91eb074d0e404b6f
Hetti Arachchilage Harshana Madhuwantha Silva <151857717+hahmsilva99@users.noreply.github.com>
2024-11-28 11:05:24 +0530

    created  java script folder for learn java script

---
+++ JAVA-SCRIPT-HUB/practice/script.js
@@ -0,0 +1,29 @@
+// hello world programm
+
+console.log("Hello world");
+
+// variables
+
+var x = 5 ;
+console.log(x);
+
+x = x+4 ;
+console.log(x);
+
+x++ ;
+console.log(x);
+
+let y = 6;
+console.log(y);
+
+// constant
+
+const c =30 ;
+console.log(c);
+  
+// sums
+
+let sum = x + y ;
+console.log(sum);
+
+ 
