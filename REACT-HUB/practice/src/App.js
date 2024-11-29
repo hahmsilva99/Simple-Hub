3b7078c722312bfca56fb057288976a7787e590c
Hetti Arachchilage Harshana Madhuwantha Silva <151857717+hahmsilva99@users.noreply.github.com>
2024-11-29 13:39:08 +0530

    updated

---
+++ REACT-HUB/practice/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Fist from './Components/Fist'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>this is my fist project</h1>
+      <Fist/>
+    </div>
+  );
+}
+
+export default App;
---
+++ REACT-HUB/practice/src/Components/Fist.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Fist.css'
+
+export default function Fist() {
+  return (
+    <div>
+        <div className="head">this is my fist component</div>
+    </div>
+  )
+}
